60c62a54ffc03cd298d577577065338232f9badb
Le Van Quy <vannn.quy@gmail.com>
2025-07-29 15:27:34 +0700

    up

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -417,7 +417,7 @@ export class AdminService implements OnModuleInit {
     }
 
     if (search) {
-      whereConditions.push('(wallet.wallet_nick_name ILIKE :search OR CAST(wallet.wallet_id AS TEXT) ILIKE :search OR wallet.wallet_solana_address ILIKE :search)');
+      whereConditions.push('(wallet.wallet_nick_name ILIKE :search OR CAST(wallet.wallet_id AS TEXT) ILIKE :search OR wallet.wallet_solana_address ILIKE :search OR wallet.bittworld_uid ILIKE :search)');
       parameters['search'] = `%${search}%`;
     }
 
